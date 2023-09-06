a2baff12dfa59539636d719806897e1ba567ac1c
David Ntwari <d.ntwar@alustudent.com>
2023-09-06 16:24:29 +0000

    castle

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -6,7 +6,7 @@ class Program
 	{
 		double percent = .7553;
 		double currency = 98765.4321;
-		// YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE.
+		Console.WriteLIne("Percent: " + percent.ToString("#0.##%"));
+		Console.WriteLine("Currency: " + currency.ToString("C2"));	
 	}
 }
-
