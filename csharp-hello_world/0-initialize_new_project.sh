4dab34e50c157e4df7818a3ccae6531004f1d872
David Ntwari <d.ntwar@alustudent.com>
2023-09-06 12:45:30 +0000

    ff

--- csharp-hello_world/0-initialize_new_project.sh
+++ csharp-hello_world/0-initialize_new_project.sh
@@ -1,11 +1,3 @@
 #!/usr/bin/env bash
 
-wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-sudo apt-get update
-
-sudo apt-get install -y dotnet-sdk-6.0
-
-
-PRJ_NAME="0-new_project"
-dotnet new console -n "$PRJ_NAME"
+dotnet new console -n "0-new_project"
