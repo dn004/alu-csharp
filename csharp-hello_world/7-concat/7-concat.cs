2161ae7e9bfabc86e0c0ede53a0870809d51b121
David Ntwari <d.ntwar@alustudent.com>
2023-09-06 15:11:29 +0000

    sh

--- csharp-hello_world/7-concat/7-concat.cs
+++ csharp-hello_world/7-concat/7-concat.cs
@@ -6,7 +6,7 @@ class Program
 	{
 		string str1 = "Holberton";
 		string str2 = "School";
-		str1 = str1 + str2;
-		Console.WriteLine($"Welcome to {str1}!");
+		
+		Console.WriteLine($"Welcome to {str1+str2}!");
 	}
 }
