230f30f2c4702065986c0b26ec0d69e5263e62b7
David Ntwari <d.ntwar@alustudent.com>
2023-09-06 12:15:24 +0000

    finally?

--- csharp-hello_world/0-initialize_new_project.sh
+++ csharp-hello_world/0-initialize_new_project.sh
@@ -1,17 +1,4 @@
 #!/usr/bin/env bash
 
-# Define the name of the new project
-project_name="new_project"
-
-# Create the folder if it doesn't exist
-mkdir -p "0-$project_name"
-
-# Change to the project directory
-cd "0-$project_name" || exit
-
-# Initialize a new C# console application project
-dotnet new console -n "$project_name"
-
-# Print a message indicating the project has been initialized
-echo "C# project '$project_name' initialized in '0-$project_name' folder."
-
+PRJ_NAME="0-new_project"
+dotnet new console -n "$PRJ_NAME"
