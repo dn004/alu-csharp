5f48d5d861c4ae4e792b41deace38c9ed4db7ab2
David Ntwari <d.ntwari@alustudent.com>
2023-09-07 17:04:29 +0200

    amry

--- csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
+++ csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for(char letter = 'a'; letter <= 'z'; letter++){
+           Console.Write(letter);
+            }
+        }
+}
