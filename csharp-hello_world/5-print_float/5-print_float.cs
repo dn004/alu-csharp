8c034696af6227b58de4382e6fb39058cd334290
David Ntwari <d.ntwari@alustudent.com>
2023-09-08 15:26:48 +0200

    n5

--- csharp-hello_world/5-print_float/5-print_float.cs
+++ csharp-hello_world/5-print_float/5-print_float.cs
@@ -1,10 +1,10 @@
-﻿ï»¿using System;
+﻿using System;
 
 class Program
 {
 	static void Main(string[] args)
         {
 		float number = 3.14159f;
-		Console.WriteLine(String.Format("{0:0.00}", number));
+		Console.WriteLine("Float: " + String.Format("{0:0.00}", number));
         }
 }
