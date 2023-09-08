523c643f56225d9433293fcb2aadd1d97a56404a
David Ntwari <d.ntwari@alustudent.com>
2023-09-08 13:48:11 +0200

    n11

---
+++ csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Line{
+    public static void PrintDiagonal(int length)
+    {
+    	for( int i = 0; i <= length; i ++)
+    	{ 
+        	if(i > 0)
+        	{
+        		Console.Write("\\");
+        	}
+        	else if(i == 0 || i < 0)
+            {
+        	    Console.WriteLine(" ");
+        	}
+        
+     	}
+    }
+}
