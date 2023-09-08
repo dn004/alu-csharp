21b3698b401c938f003a2bbaca033d68d985180c
David Ntwari <d.ntwari@alustudent.com>
2023-09-08 11:06:44 +0200

    gg

--- csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
+++ csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -7,13 +7,13 @@ class Program
         Random rndm = new Random();
         int number = rndm.Next(-10, 10);
 	if (number > 0){
-	Console.WriteLine("is positive");
+	Console.WriteLine($"{number}is positive");
     }
 	else if (number < 0){
-        Console.WriteLine("is negative");
+        Console.WriteLine($"{number}is negative");
     }
 	else if (number == 0){
-        Console.WriteLine("is zero");
+        Console.WriteLine($"{number}is zero");
     }
 }
 }
