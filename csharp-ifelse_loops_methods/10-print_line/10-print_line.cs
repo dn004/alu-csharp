8438d0a14e5a1c66f1d7ac2a719cc1140733fd27
David Ntwari <d.ntwari@alustudent.com>
2023-09-08 13:38:09 +0200

    n10

---
+++ csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Line{
+    public static void PrintLine(int length)
+    {
+    	for( int i = 0; i <= length; i ++)
+    	{ 
+        	if(i > 0)
+        	{
+        		Console.Write("_");
+        	}
+        	else
+            {
+        	    Console.WriteLine(" ");
+        	}
+        
+     	}
+    }
+}
