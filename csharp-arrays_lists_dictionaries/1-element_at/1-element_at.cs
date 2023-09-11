7821a79de3422825741d230071584ec2e1bf0586
David Ntwari <d.ntwari@alustudent.com>
2023-09-11 12:17:48 +0200

    n1

---
+++ csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -0,0 +1,16 @@
+﻿    using System;
+
+    class Array{
+        public static int elementAt(int[] array, int index){
+
+            if (index < 0 ||index > array.Length){
+                Console.WriteLine ("Index Out Of Range");
+                return -1;
+            }
+            else{
+                int element = array[index];
+                return element;
+                
+            }
+        }
+    }
