3371658f7869aee3ef2d75aa3adcade85ed4c437
David Ntwari <d.ntwari@alustudent.com>
2023-09-11 16:43:36 +0200

    n12

---
+++ csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        var sortedDict = myDict.OrderBy(pair => pair.Key);
+
+        foreach (var kvp in sortedDict)
+        {
+            Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+        }
+    }
+}
