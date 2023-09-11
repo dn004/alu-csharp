009d7c46c81c156b7e44ef34fb84075e49dad93b
David Ntwari <d.ntwari@alustudent.com>
2023-09-11 16:38:49 +0200

    n11

---
+++ csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Dictionary{
+    public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict){
+
+        Dictionary<string, int> newDict = new Dictionary<string, int>();
+
+        foreach (KeyValuePair<string, int> kvp in myDict){
+
+            newDict[kvp.Key] = kvp.Value * 2; 
+        }
+        return newDict;
+    }
+}
