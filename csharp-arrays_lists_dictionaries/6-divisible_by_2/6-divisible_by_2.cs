beeb62ad3bfb209d4fe49d892cb7229f86af3a89
David Ntwari <d.ntwari@alustudent.com>
2023-09-11 14:10:36 +0200

    n6

---
+++ csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class List{
+    public static List<bool> DivisibleBy2(List<int> myList){
+        List<bool> result = new List<bool>();
+        for(int i = 0; i < myList.Count; i++){
+            if (myList(i) % 2 == 0){
+                result(i) = true;
+            }
+            else {
+                result(i) = false;
+            }
+        }
+    return result;
+    }
+}
