fbc705ba3fa9fcd44c4cd8cd67be24bfe69a5e51
David Ntwari <d.ntwari@alustudent.com>
2023-09-11 13:52:45 +0200

    n5

---
+++ csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class List {
+    public static int MaxInteger(List<int> myList){
+        if (myList.Count == 0)
+        {
+            Console.WriteLine("List is empty");
+            return -1;
+        }
+        else
+        {
+            int lrgNum = myList[0];
+            for (int i = 1; i < myList.Count; i++)
+            {
+                if (myList[i] > lrgNum)
+                {
+                    lrgNum = myList[i];
+                }
+            }
+        return lrgNum;
+    
+        }
+    }
+}
