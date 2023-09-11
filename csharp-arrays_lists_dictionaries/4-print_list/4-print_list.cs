6da78dce037108af4d03eb5d2feb7df1ae3e08ff
David Ntwari <d.ntwari@alustudent.com>
2023-09-11 13:26:44 +0200

    n4

---
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List {
+    public static List<int> CreatePrint(int size){
+        List<int> myList = new List<int>();
+
+        if (size == 0 || size < 0){
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        else{
+            for (int i = 0; i = size; i ++){
+                myList.Add(i);
+                return myList;
+            }
+        }
+    }
+}
