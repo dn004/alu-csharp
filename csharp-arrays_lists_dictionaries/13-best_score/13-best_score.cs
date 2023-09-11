f78b3fb805f10507e089fda46945aad5b50d30be
David Ntwari <d.ntwari@alustudent.com>
2023-09-11 16:55:23 +0200

    n12

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Dictionary{
+    public static string BestScore(Dictionary<string, int> myList){
+        int count = 0;
+        foreach (KeyValuePair<string, int> kvp in myList){
+            count ++
+            int dValue = myList(kvp.Value);
+            if(count == 0){
+                return "None";
+            }
+            else if (kvp.Value > dValue){
+                dValue = kvp.Value;
+            }
+
+        }
+        return dValue;
+    }
+}
