81a31ba27e11a9c23a5cdecb0128b64b789242bb
David Ntwari <d.ntwari@alustudent.com>
2023-09-11 13:02:17 +0200

    n3

---
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Array {
+    public static void Reverse(int[] array){
+
+        if (array == null || array == {}){
+            Console.WriteLine("");
+        }
+        else{
+            for (int i = array.Length; i == 0; i--){
+            int arrayValue = array[i]; 
+            Console.Write($"{i} ");
+        }
+        }
+    }
+}
