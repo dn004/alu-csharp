88f52c838739c0d41206e937c6dfa8bca53c2104
David Ntwari <d.ntwari@alustudent.com>
2023-09-11 16:23:18 +0200

    n9

---
+++ csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
@@ -0,0 +1,14 @@
+﻿using system;
+
+class Dictionary
+{
+    public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value)
+    {
+        foreach (KeyValuePair<string, string> kvp in myDict){
+            if(myDict[key] == kvp.Key || myDict[key] != kvp.Key){
+                myDict[key] = value;
+            }
+        }
+    return myDict;
+    }
+}
