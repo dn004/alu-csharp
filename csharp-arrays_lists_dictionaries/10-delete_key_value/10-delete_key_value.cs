41a8232e65bed0465d6d7d2bf0e9e56f1e3ac554
David Ntwari <d.ntwari@alustudent.com>
2023-09-11 16:30:46 +0200

    n10

---
+++ csharp-arrays_lists_dictionaries/10-delete_key_value/10-delete_key_value.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Dictionary{
+    public static Dictionary<string, string> DeleteKeyValue(Dictionary<string, string> myDict, string key){
+        
+        foreach (KeyValuePair<string, string> kvp in myDict){
+            if(myDict[key] == kvp.Key){
+                myDict.Remove[key];
+            }
+        }
+        return myDict;
+    }
+}
