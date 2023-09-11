ce797b9f44fed690aa33b851a998075ec317ff90
David Ntwari <d.ntwari@alustudent.com>
2023-09-11 17:04:19 +0200

    n14g

---
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] myArray = new int[5, 5];
+
+        for (int row = 0; row < 5; row++)
+        {
+            for (int col = 0; col < 5; col++)
+            {
+                myArray[row, col] = 0;
+            }
+        }
+
+        myArray[2, 2] = 1;
+
+        for (int row = 0; row < 5; row++)
+        {
+            for (int col = 0; col < 5; col++)
+            {
+                Console.Write(myArray[row, col] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
