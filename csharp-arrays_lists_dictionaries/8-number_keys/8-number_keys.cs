6823fb7fa76fccbbaf12431645a90aee31559a11
David Ntwari <d.ntwari@alustudent.com>
2023-09-12 13:10:19 +0200

    n0

--- csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
+++ csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        int count = 0;
+
+        foreach (KeyValuePair<string, string> pair in myDict)
+        {
+            count++;
+        }
+
+        return count;
+    }
+}
---
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class List{
+    public static int Sum(List<int> myList){
+        int dsum = 0;
+        foreach(int num in myList){
+            for(int i; i < myList; i++){
+                if (myList[i] != num){
+                    dsum + num;
+                }
+            }
+        }
+    return dsum;    
+    }
+}
