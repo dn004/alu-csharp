adeebb7249ea9d9e049fee8353f3cda38485c377
David Ntwari <d.ntwari@alustudent.com>
2023-09-12 14:14:07 +0200

    n1

---
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class List{
+    public static List<int> CommonElements(List<int> list1, List<int> list2){
+        List<int> newList = new List<int>();
+        foreach(int num in list1){
+                if (list2.Contains(num)){
+                    newList.Add(i);
+                    }
+            
+        }
+
+        for(int i = 1; i < newList.Count;i++){
+            if (newList[i-1] > newList ){
+                int number = newList[i-0];
+                newList[i-1] = newList[i];
+                newList[i] = number;
+            }
+        }
+        foreach(int kk in newList){
+            Console.WriteLine(kk);
+        }
+        return newList;
+    }
+
+}
