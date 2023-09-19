e4100970a9b8023d2e5028ca1f87cc3a3842af58
David Ntwari <d.ntwari@alustudent.com>
2023-09-19 13:22:11 +0200

    n1hashed & n2 done

--- csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -3,15 +3,16 @@
 class List{
     public static List<int> CommonElements(List<int> list1, List<int> list2){
         List<int> newList = new List<int>();
-        foreach(int num in list1){
-                if (list2.Contains(num)){
+        HashSet<int> HashList1 = new HashSet<int>(list1);
+        HashSet<int> HastList2 = new HashSet<int>(list2);
+
+        HashList1.IntersectWith(HastList2);
+        
+        foreach(int num in HashList1)
+            {
                     newList.Add(num);
-                    }
-            
+            }
+            newList.Sort();
+            return newList;
         }
-
-        newList.Sort();
-        return newList;
     }
-
-}
---
+++ csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        List<int> newList = new List<int>();
+        HashSet<int> HashList1 = new HashSet<int>(list1);
+        HashSet<int> HastList2 = new HashSet<int>(list2);
+        int num;
+        bool checkHash = HashList2.Contains(num);
+        foreach(num in HashList1){
+                if (checkHash = false){
+                    newList.Add(num);
+                    }
+        }
+        newList.Sort();
+        return newList;
+    }
+}
