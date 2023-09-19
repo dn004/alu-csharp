2d017985c3c851ff07c2c9e4dd7a120855b2a382
David Ntwari <d.ntwari@alustudent.com>
2023-09-19 15:19:57 +0200

    n3 tested true

---
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine($"Number of items: {aStack.Count}");
+        
+        if(aStack.Count == 0)
+        {
+            Console.WriteLine("Stack is empty");
+        }
+        else
+        {
+            Console.WriteLine($"Top item:{aStack.Peek()}");
+        }
+        bool containsSearch = aStack.Contains(search);
+        Console.WriteLine($"Stack contains {search}: {containsSearch}");
+
+         for(int i = 0; i <= aStack.Count; i++)
+         {
+            if(containsSearch = true)
+            {
+                aStack.Pop();
+            }
+         }
+
+        aStack.Push(newItem);
+
+        return aStack;
+    }
+}
