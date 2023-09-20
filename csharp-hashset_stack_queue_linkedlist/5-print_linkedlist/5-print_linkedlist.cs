78271cefdf8e1fdec58d870d53b29baf3a22f96a
David Ntwari <d.ntwari@alustudent.com>
2023-09-20 11:27:22 +0200

    n5 tested true

--- csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
+++ csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -2,20 +2,34 @@
 
 class LList
 {
+    static void Main(string[] args)
+    {
+        LinkedList<int> llist;
+        int size;
+
+        size = 8;
+
+        llist = LList.CreatePrint(size);
+        Console.WriteLine("-------------");
+        Console.WriteLine("Linked List Length: " + llist.Count);
+    }
     public static LinkedList<int> CreatePrint(int size)
     {
         LinkedList<int> myllist = new LinkedList<int>();
 
-        if(size >= 0)
+        if(size <= 0)
         {
             return myllist;
         }
         else
         {
-            for(int i; i == count; i ++)
+            for(int i = 0; i<size; i++)
             {
                 myllist.AddLast(i);
+                Console.WriteLine(i);
             }
         }
+
+        return myllist;
     }
 }
