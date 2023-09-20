2de29901724842b518cd466787c5ea0bdcdb1347
David Ntwari <d.ntwari@alustudent.com>
2023-09-20 12:17:17 +0200

    n8 tested true

---
+++ csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    static void Main(string[] args)
+    {
+        LinkedList<int> myLList = new LinkedList<int>();
+
+        myLList.AddLast(1);
+        myLList.AddLast(4);
+        myLList.AddLast(9);
+        myLList.AddLast(16);
+        myLList.AddLast(25);
+        myLList.AddLast(36);
+        myLList.AddLast(49);
+
+        Console.WriteLine("Node Position: " + LList.FindNode(myLList, 16));
+        Console.WriteLine("Node Position: " + LList.FindNode(myLList, 1));
+        Console.WriteLine("Node Position: " + LList.FindNode(myLList, -10));
+    }
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int count = -1;
+        foreach(int noode in myLList)
+        {
+            count++;
+
+            if(noode == value)
+            {
+                return count;
+            }
+        }
+
+        return -1;
+    }
+}
