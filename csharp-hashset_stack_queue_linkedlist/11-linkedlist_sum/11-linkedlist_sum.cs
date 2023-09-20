1a4c214c0dd43a00c1cba3da5f51a8be39c3d03d
David Ntwari <d.ntwari@alustudent.com>
2023-09-20 14:08:46 +0200

    n11 tt

---
+++ csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class LList
+{
+    static void Main(string[] args)
+    {
+        LinkedList<int> myLList = new LinkedList<int>();
+
+        myLList.AddLast(21);
+        myLList.AddLast(9);
+        myLList.AddLast(-8);
+        myLList.AddLast(54);
+        myLList.AddLast(23);
+        myLList.AddLast(-6);
+        myLList.AddLast(5);
+
+        Console.WriteLine(LList.Sum(myLList));
+    }
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int sum = 0;
+        foreach (int value in myLList)
+        {
+            sum += value;
+        }
+        return sum;
+    }
+}
