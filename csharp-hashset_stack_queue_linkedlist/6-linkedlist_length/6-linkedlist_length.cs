6e4b15ec517613b5d158dceec2a0fe1f21d868f9
David Ntwari <d.ntwari@alustudent.com>
2023-09-20 11:16:16 +0200

    n6 tested true

---
+++ csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class LList
+{
+    static void Main(string[] args)
+    {
+        LinkedList<int> myLList = new LinkedList<int>();
+
+        myLList.AddLast(972);
+        myLList.AddLast(0);
+        myLList.AddLast(8);
+        myLList.AddLast(98);
+        myLList.AddLast(-4);
+        myLList.AddLast(8);
+
+        Console.WriteLine(LList.Length(myLList));
+    }
+    public static int Length(LinkedList<int> myLList)
+    {
+        int count = 0;
+        foreach(int integer in myLList)
+        {
+            count++;
+        }
+
+        return count;
+    }
+}
