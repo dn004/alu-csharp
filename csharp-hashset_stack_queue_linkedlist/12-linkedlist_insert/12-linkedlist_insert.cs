b3ed9ced0800731f9ec23bc20462c12721dd40c3
David Ntwari <d.ntwari@alustudent.com>
2023-09-20 14:30:33 +0200

    n12 tt

---
+++ csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class LList
+{
+    static void Main(string[] args)
+    {
+        LinkedList<int> myLList = new LinkedList<int>();
+        LinkedListNode<int> current;
+
+        myLList.AddLast(1);
+        myLList.AddLast(4);
+        myLList.AddLast(9);
+        myLList.AddLast(16);
+        myLList.AddLast(25);
+        myLList.AddLast(36);
+        myLList.AddLast(49);
+
+        current = myLList.First;
+        while (current != null)
+        {
+            Console.WriteLine(current.Value);
+            current = current.Next;
+        }
+
+        Console.WriteLine("------------------");
+        LList.Insert(myLList, 21);
+
+        current = myLList.First;
+        while (current != null)
+        {
+            Console.WriteLine(current.Value);
+            current = current.Next;
+        }
+    }
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {   
+        LinkedListNode<int> Noode = new LinkedListNode<int>(n);
+        Noode = myLList.First;
+        while(Noode != null)
+        {
+            if(Noode.Value >= n)
+            {
+                myLList.AddBefore(Noode, n);
+                return Noode.Previous;
+            }
+              Noode = Noode.Next;
+        }
+           
+            return Noode;
+    }
+}
