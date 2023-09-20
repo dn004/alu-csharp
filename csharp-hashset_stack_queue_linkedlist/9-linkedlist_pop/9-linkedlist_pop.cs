5c401963b5499268e9aaae9585adcb36af2037f8
David Ntwari <d.ntwari@alustudent.com>
2023-09-20 12:48:09 +0200

    n9 tested true

---
+++ csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    static void Main(string[] args)
+    {
+        LinkedList<int> myLList = new LinkedList<int>();
+
+        myLList.AddLast(11);
+        myLList.AddLast(3);
+        myLList.AddLast(-9);
+        myLList.AddLast(47);
+        myLList.AddLast(0);
+        myLList.AddLast(-9);
+
+        Console.WriteLine(LList.Pop(myLList));
+    }
+    public static int Pop(LinkedList<int> myLList)
+    {
+        LinkedListNode<int> firstNode = myLList.First;
+        int valueNode = firstNode.Value;
+
+        if (firstNode != null)
+        {
+            myLList.Remove(firstNode);
+        }
+        else
+        {
+            return 0;
+        }
+        return valueNode;
+    }
+}
