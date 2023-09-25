a20b31c305d883937f67edee718c842069a1340a
David Ntwari <d.ntwari@alustudent.com>
2023-09-25 13:04:41 +0200

    removing main tester n2

--- csharp-exceptions/2-divide_lists/2-divide_lists.cs
+++ csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -2,17 +2,6 @@
 
 class List
 {
-    static void Main(string[] args)
-    {
-        List<int> list1 = new List<int>() {1, 20, 16, 15, 54};
-        List<int> list2 = new List<int>() {1, 0, 2, 3};
-        List<int> result;
-
-        result = List.Divide(list1, list2, 5);
-
-        foreach (int i in result)
-            Console.WriteLine(i);
-    }
     public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
     {
         List<int> dvdlist = new List<int>{};
