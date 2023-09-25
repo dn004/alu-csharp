7a9879e8a5f55118511296a63d86b29f1a453bde
David Ntwari <d.ntwari@alustudent.com>
2023-09-25 11:48:28 +0200

    n0 tt

--- csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
+++ csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -2,22 +2,6 @@
 
 class List
 {
-    static void Main(string[] args)
-    {
-        List<int> myList = new List<int>() {1, 2, 3, 4, 5};
-        int count;
-
-        count = List.SafePrint(myList, myList.Count);
-        Console.WriteLine("Number of integers printed: " + count);
-        Console.WriteLine();
-
-        count = List.SafePrint(myList, myList.Count - 2);
-        Console.WriteLine("Number of integers printed: " + count);
-        Console.WriteLine();
-
-        count = List.SafePrint(myList, myList.Count + 2);
-        Console.WriteLine("Number of integers printed: " + count);
-    }
     public static int SafePrint(List<int> myList, int n)
     {
         int count = 0;
