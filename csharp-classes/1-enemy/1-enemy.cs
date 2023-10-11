15d355d9b8a74759e952adba8dff1bd2bdd45368
David Ntwari <d.ntwari@alustudent.com>
2023-10-11 14:59:34 +0200

    remove main

--- csharp-classes/1-enemy/1-enemy.cs
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -1,13 +1,4 @@
 ﻿using System;
-class Program
-{
-    static void Main(string[] args)
-    {
-        Enemies.Zombie newObject = new Enemies.Zombie();
-
-        Console.WriteLine("newObject is of type {0} and has a total of {1} health", newObject.GetType(), newObject.health);
-    }
-}
 
 namespace Enemies
 {
