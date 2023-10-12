a726dc3908666a2cdf262ca0d0dc2bea9932d26f
David Ntwari <d.ntwari@alustudent.com>
2023-10-12 12:18:24 +0200

    n2 tt

---
+++ csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Enemies
+{
+    class Zombie
+    {
+        public int  health;
+        public int Health{
+            get {return health;}
+            set
+            {health = value;}
+        }  
+
+        public Zombie()
+        {
+            Health = 0;
+        }
+
+        public Zombie(int value)
+        {
+            if (value >= 0)
+                {
+                    health = value;
+                }
+            else if (value < 0)
+                {
+                    throw new ArgumentException("Health must be greater than or equal to 0");
+                }
+        }
+    }
+
+
+}
