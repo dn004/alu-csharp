444a6345378631432618d77a116aafcfcfefd1c3
David Ntwari <d.ntwari@alustudent.com>
2023-10-13 15:49:44 +0200

    all

--- csharp-classes/4-enemy/4-enemy.cs
+++ csharp-classes/4-enemy/4-enemy.cs
@@ -4,19 +4,29 @@ namespace Enemies
 {
     class Zombie
     {
-        private int  health;
-        public int Health{
-            get {return health;}
-            set
-            {health = value;}
-        } 
+        private int health;
+        public int Health
+        {
+            get { return health; }
+            set { health = value; }
+        }
 
         private string name = "(No Name)";
         public string Name
         {
-            get {return name;}
-            set{name = value;}
-        } 
+            get { return name; }
+            set
+            {
+                if (name == null)
+                {
+                    name = "(No Name)";
+                }
+                else
+                {
+                    name = value;
+                }
+            }
+        }
 
         public Zombie()
         {
@@ -26,13 +36,13 @@ namespace Enemies
         public Zombie(int value)
         {
             if (value >= 0)
-                {
-                    health = value;
-                }
+            {
+                health = value;
+            }
             else if (value < 0)
-                {
-                    throw new ArgumentException("Health must be greater than or equal to 0");
-                }
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
         }
 
         public int GetHealth()
