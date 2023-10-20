ba206d165a9a6c586c44ff075b526e57ce2d970e
David Ntwari <d.ntwari@alustudent.com>
2023-10-20 08:46:14 +0200

    palindromes

---
+++ csharp-tdd/3-palindrome/Text/Text.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using System;
+
+namespace Text
+{
+    
+    public class Str
+    {
+        public static bool IsPalindrome(string s)
+        {
+            if (s == string.Empty || s == " ")
+            {
+                return true;
+            }
+            
+            string orignal = s.ToLower();
+            IEnumerable<char> reverseOriginal = s.Reverse();
+            string OriginalLast = null;
+            string OriginalFirst = null;
+            
+            // remove all spaces
+            foreach (char l in reverseOriginal)
+            {
+                if (l == ' ')
+                {
+                    continue;
+                }
+                OriginalLast += l;
+            }
+
+            foreach (char f in orignal)
+            {
+                if (f == ' ')
+                {
+                    continue;
+                }
+                else
+                {
+                    OriginalFirst += f;
+                }
+            }
+
+            if (OriginalFirst == OriginalLast.ToLower())
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
