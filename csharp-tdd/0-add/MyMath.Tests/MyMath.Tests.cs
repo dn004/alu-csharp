185a48aae163312c9f0b06e273ffecf28504f584
David Ntwari <d.ntwari@alustudent.com>
2023-10-20 07:56:52 +0200

    n0

---
+++ csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,37 @@
+using NUnit.Framework;
+using MyMath;
+
+namespace Tests
+{
+    /// <summary>
+    /// This class contains test methods for the 'Add' operation in the 'Opps' class.
+    /// </summary>
+    [TestFixture]
+    public class Test_NumberAddition
+    {
+        private Opps _myMath;
+
+        /// <summary>
+        /// This method is called before each test to set up the test environment.
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+            _myMath = new Opps();
+        }
+
+        /// <summary>
+        /// Test if addition of two numbers is equal to 4.
+        /// </summary>
+        [Test]
+        public void AdditionOperation()
+        {
+            bool state = false;
+            if (Opps.Add(2, 2) == 4)
+            {
+                state = true;
+            }
+            Assert.IsTrue(state, "State is True");
+        }
+    }
+}
---
+++ csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyMath
+{
+    /// <summary>
+    /// This class provides mathematical operations.
+    /// </summary>
+    public class Opps
+    {
+        /// <summary>
+        /// Returns the addition of two integers.
+        /// </summary>
+        /// <param name="a">The first integer to be added.</param>
+        /// <param name="b">The second integer to be added.</param>
+        /// <returns>The sum of the two integers.</returns>
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
