f85e01e0fcc60e9b0caf6e225f019c800940ceca
David Ntwari <d.ntwari@alustudent.com>
2023-11-03 11:54:26 +0200

    n3 tt

---
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+
+
+class Obj 
+{
+    public static void Print(object myObj)
+    {
+
+        TypeInfo t =  myObj.GetType().GetTypeInfo();
+        IEnumerable<PropertyInfo> pList = t.GetProperties();
+        Console.WriteLine($"{t.Name} Properties:");
+
+
+        foreach(PropertyInfo n in pList)
+        {
+         
+            Console.WriteLine(n.Name);
+        }
+
+        IEnumerable<MethodInfo> nMethod = t.GetMethods();
+        Console.WriteLine($"{t.Name} Methods:");
+
+        foreach(MethodInfo m in nMethod)
+        {
+            if(m.DeclaringType == myObj.GetType())
+            {
+                 Console.WriteLine(m.Name);
+            }
+        }
+
+    }
+}
