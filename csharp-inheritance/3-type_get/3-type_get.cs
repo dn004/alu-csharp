3749cb64f593d70078f7b412f4cff7a949a9598f
David Ntwari <d.ntwari@alustudent.com>
2023-11-06 11:35:23 +0200

    documentation

--- csharp-inheritance/3-type_get/3-type_get.cs
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -1,32 +1,34 @@
 ﻿using System.Reflection;
 
-
-class Obj 
+/// <summary>
+/// Provides utility methods for inspecting object properties and methods.
+/// </summary>
+class Obj
 {
+    /// <summary>
+    /// Prints the properties and methods of the specified object.
+    /// </summary>
+    /// <param name="myObj">The object to inspect.</param>
     public static void Print(object myObj)
     {
-
-        TypeInfo t =  myObj.GetType().GetTypeInfo();
+        TypeInfo t = myObj.GetType().GetTypeInfo();
         IEnumerable<PropertyInfo> pList = t.GetProperties();
         Console.WriteLine($"{t.Name} Properties:");
 
-
-        foreach(PropertyInfo n in pList)
+        foreach (PropertyInfo n in pList)
         {
-         
             Console.WriteLine(n.Name);
         }
 
         IEnumerable<MethodInfo> nMethod = t.GetMethods();
         Console.WriteLine($"{t.Name} Methods:");
 
-        foreach(MethodInfo m in nMethod)
+        foreach (MethodInfo m in nMethod)
         {
-            if(m.DeclaringType == myObj.GetType())
+            if (m.DeclaringType == myObj.GetType())
             {
-                 Console.WriteLine(m.Name);
+                Console.WriteLine(m.Name);
             }
         }
-
     }
 }
--- csharp-inheritance/4-inherit/4-inherit.cs
+++ csharp-inheritance/4-inherit/4-inherit.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 
+/// <summary>
+/// Represents a base class for animals.
+/// </summary>
 public class Animal
 {
-
+    // This class can be used as a base class for various types of animals.
 }
 
+/// <summary>
+/// Represents a specific type of animal, a dog.
+/// </summary>
 public class Dog : Animal
 {
-
+    // This class represents a specific type of animal, a dog, which inherits from the Animal class.
 }
