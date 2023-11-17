e4f2357c51fe7e31109599c667ac15474e0cd863
David Ntwari <d.ntwari@alustudent.com>
2023-11-17 15:34:07 +0200

    23-33

---
+++ csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+public class MatrixTransformation
+{
+    // Applies a shear along the X-axis to a 2D matrix of points.
+    public static double[,] ShearX(double[,] matrix, double shx)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+        double[,] result = new double[rows, cols];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                double x = matrix[i, j];
+                double y = i; // Assuming each row corresponds to a Y coordinate
+                // Applying shear transformation
+                result[i, j] = x + shx * y;
+            }
+        }
+
+        return result;
+    }
+
+    // Applies a shear along the Y-axis to a 2D matrix of points.
+    public static double[,] ShearY(double[,] matrix, double shy)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+        double[,] result = new double[rows, cols];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                double x = j; // Assuming each column corresponds to an X coordinate
+                double y = matrix[i, j];
+                // Applying shear transformation
+                result[i, j] = shy * x + y;
+            }
+        }
+
+        return result;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        double[,] matrix = {
+            { 1, 2 },
+            { 3, 4 }
+        };
+
+        double shearFactorX = 2; // Shear along X-axis
+        double shearFactorY = 2; // Shear along Y-axis
+
+        double[,] shearedMatrixX = MatrixTransformation.ShearX(matrix, shearFactorX);
+        double[,] shearedMatrixY = MatrixTransformation.ShearY(matrix, shearFactorY);
+
+        // Output the sheared matrix
+        Console.WriteLine("Sheared Matrix along X:");
+        for (int i = 0; i < shearedMatrixX.GetLength(0); i++)
+        {
+            for (int j = 0; j < shearedMatrixX.GetLength(1); j++)
+            {
+                Console.Write($"{shearedMatrixX[i, j]} ");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Sheared Matrix along Y:");
+        for (int i = 0; i < shearedMatrixY.GetLength(0); i++)
+        {
+            for (int j = 0; j < shearedMatrixY.GetLength(1); j++)
+            {
+                Console.Write($"{shearedMatrixY[i, j]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+
+
+class Program
+{
+    public static void Main(string[] arg)
+    {
+       double[,] testMatrix = {{11, 9, 7}, {2, 13, 1}, {4, 0, 17}};
+        Console.WriteLine(" " + MatrixMath.Determinant(testMatrix));
+    }
+}
+
+class MatrixMath
+{
+
+    public static double Determinant(double[,] matrix)
+    {
+
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        if((rows == 3 && cols == 3 ) || (rows == 2 && cols == 2))
+        {
+
+            if(rows == 2)
+            {
+                return Math.Round(((matrix[0,0] * matrix[1,1]) -  (matrix[0,1] * matrix[1,0])),2);
+            }
+
+            if(rows == 3)
+            {
+                double x = matrix[0,0] * ((matrix[1,1] * matrix[2,2]) - (matrix[2,1] * matrix[1,2]));
+                double y = matrix[0,1] * ((matrix[1,0] * matrix[2,2]) - (matrix[2,0] * matrix[1,2]));
+                double z = matrix[0,2] * ((matrix[1,0] * matrix[2,1]) - (matrix[2,0] * matrix[1,1]));
+
+                return Math.Round((x - y - z),2);
+            }
+        }
+
+        return -1;
+    }
+}
