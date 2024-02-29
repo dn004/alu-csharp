9294c2e4b6ea7f7f9fb4bd5b969cfa3d5bea95fc
dn004 <d.ntwari@alustudent.com>
2024-02-29 12:26:49 +0200

    0-queue

---
+++ csharp-generics/0-queue/queue.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/// <summary>
+/// Represents a generic queue.
+/// </summary>
+/// <typeparam name="T">The type of elements in the queue.</typeparam>
+public class Queue<T>
+{
+    private Type type;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Queue{T}"/> class.
+    /// </summary>
+    public Queue()
+    {
+        type = typeof(T);
+    }
+
+    /// <summary>
+    /// Method to check the type of the Queue.
+    /// </summary>
+    /// <returns>The type of elements in the queue.</returns>
+    public Type CheckType()
+    {
+        return type;
+    }
+}
