a9423369a86ac0ade78225c5dbb453ac4efe7e0b
dn004 <d.ntwari@alustudent.com>
2024-03-01 10:49:43 +0200

    0 abstract

--- csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using System;
+
+/// <summary>
+/// Represents a base class with a name property.
+/// </summary>
+public abstract class Base
+{
+    /// <summary>
+    /// Gets or sets the name of the base entity.
+    /// </summary>
+    public string name { get; set; }
+
+    /// <summary>
+    /// Overrides the ToString() method to return the name and type of the entity.
+    /// </summary>
+    /// <returns>A string representing the name and type of the entity.</returns>
+    public override string ToString()
+    {
+        return $"{name} is a {GetType().Name}";
+    }
+}
