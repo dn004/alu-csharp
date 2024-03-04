ea026b1b2eb4a17fbe82b7324c4f09f0674599cf
dn004 <d.ntwari@alustudent.com>
2024-03-04 12:01:50 +0200

    peek done

--- csharp-generics/3-peek/obj/Debug/net8.0/3-peek.AssemblyInfo.cs
+++ csharp-generics/3-peek/obj/Debug/net8.0/3-peek.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("3-peek")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67c6a1b79f756754faeb2def44394255d4113c6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0cc7f4429b038d64c6cbbdd3dd8c7904aaa24e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("3-peek")]
 [assembly: System.Reflection.AssemblyTitleAttribute("3-peek")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
