7fd1ffed97f83b02a5ee87604b96c9ac2562ebe3
dn004 <d.ntwari@alustudent.com>
2024-03-05 12:42:56 +0200

    universal health achieved i guess

---
+++ csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+/// <summary>
+/// Represents a player entity with health points.
+/// </summary>
+public class Player
+{
+    private string? name;
+    private float maxHp;
+    private float hp;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Player"/> class with default values.
+    /// </summary>
+    /// <param name="name">The name of the player.</param>
+    /// <param name="maxHp">The maximum health points of the player.</param>
+    public Player(string? name = "Player", float maxHp = 100f)
+    {
+        if (maxHp <= 0)
+        {
+            this.maxHp = 100f;
+            System.Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        else
+        {
+            this.maxHp = maxHp;
+        }
+
+        this.name = name;
+        hp = this.maxHp;
+    }
+
+    /// <summary>
+    /// Prints the current health of the player.
+    /// </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{name} has {hp} / {maxHp} health");
+    }
+}
+
+
+
+
+
+/*
+class Program
+{
+    static void Main(string[] args)
+    {
+        Player player1 = new Player("Electric Mouse", 500f);
+        Player player2 = new Player("Water Turtle");
+
+        player1.PrintHealth();
+        player2.PrintHealth();
+    }
+}
+*/
