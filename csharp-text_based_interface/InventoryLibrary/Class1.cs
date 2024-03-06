63f8679762e428acb1a154912fda2acd9b9a3a20
dn004 <d.ntwari@alustudent.com>
2024-03-06 11:46:58 +0200

    0 done

---
+++ csharp-text_based_interface/InventoryLibrary/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class BaseClass
+{
+    string? id;
+    DateTime date_created;
+    DateTime date_updated;
+}
---
+++ csharp-text_based_interface/README.md
@@ -0,0 +1,2 @@
+# Text Based Interface.
+## Inventory Managment
