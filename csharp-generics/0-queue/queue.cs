2c80901358152fb28686e173a94c71ed301f4655
dn004 <d.ntwari@alustudent.com>
2024-06-24 09:19:19 +0200

    ok

--- csharp-generics/0-queue/queue.cs
+++ csharp-generics/0-queue/queue.cs
@@ -16,7 +16,7 @@ public class Queue<T>  {
 
 
 
-
+/*
 class Program
 {
     static void Main(string[] args)
@@ -28,3 +28,4 @@ class Program
         Console.WriteLine($"{nameof(myObjQ)} Queue Type: " + myObjQ.CheckType());
     }
 }
+*/
