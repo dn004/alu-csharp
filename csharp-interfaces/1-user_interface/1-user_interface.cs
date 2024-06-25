f8d316837c74743ec409dd4164eec513a2dcc971
dn004 <d.ntwari@alustudent.com>
2024-06-25 08:25:56 +0200

    change sdk

--- csharp-interfaces/1-user_interface/1-user_interface.cs
+++ csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -2,86 +2,83 @@
 using System.Collections.Generic;
 using System.Reflection;
 
+
 /// <summary>
-/// Base class representing an object with a name.
+/// interace for interactions
 /// </summary>
-public abstract class Base
-{
-    /// <summary>
-    /// The name of the object.
-    /// </summary>
-    public string? name;
+public interface IInteractive{
 
     /// <summary>
-    /// Returns a string representation of the object.
+    /// interact method
     /// </summary>
-    /// <returns>A string representation of the object.</returns>
-    public override string ToString()
-    {
+    public void Interact();
+}
+
+
+/// <summary>
+/// interface for breakables
+/// </summary>
+public interface IBreakable {
+
+    // durability
+    public int durability { get ; set;}
+
+    /// <summary>
+    /// breaking stuff
+    /// </summary>
+    public void Break();
+}
+
+
+/// <summary>
+/// collecting stuff
+/// </summary>
+public interface ICollectable{
+
+    // collecting
+    public bool isCollected { get ; set; }
+    /// <summary>
+    /// for collecting objects.
+    /// </summary>
+    public void Collect();
+}
+
+/// <summary>
+/// main class for testing
+/// </summary>
+public class TestObject : Base, IInteractive, IBreakable, ICollectable{
+    public int durability { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public bool isCollected { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    public void Interact(){
+
+    }
+
+    public void Break(){
+
+    }
+
+    public void Collect(){
+
+    }
+
+}
+
+
+
+/// <summary>
+/// base class for everythign.
+/// </summary>
+
+public abstract class Base{
+
+    public String? name { get ; set; }
+
+    public override String ToString(){
         return $"{name} is a {this.GetType()}";
     }
-
-
-
-    /// <summary>
-    /// Represents an interactive object.
-    /// </summary>
-
-    public interface IInteractive
-    {
-        public void Interact();
-    }
-
-
-
-    /// <summary>
-    /// Represents a breakable object.
-    /// </summary>
-
-    public interface IBreakable
-    {
-        public int durability{ get ; set;}
-        public void Break();
-    }
-
-
-
-    /// <summary>
-    /// Represents a collectable object.
-    /// </summary>
-
-    public interface ICollectable
-    {
-        public bool isCollected{ get ; set;}
-        public void Collect();
-    }
 }
 
-
-
-
-/// <summary>
-/// Represents a test object that implements interfaces.
-/// </summary>
-
-class TestObject : Base, Base.IInteractive, Base.IBreakable, Base.ICollectable
-{
-
-    public int durability;
-    public bool isCollected;
-
-    public void Interact()
-    {
-    }
-
-    public void Break()
-    {
-    }
-
-    public void Collect()
-    {
-    }
-}
 
 
 
